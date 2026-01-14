ae981d6bb59b5766417eff583fffd654b200ae6f
Jonas Ha <jonas-ha@outlook.com>
2026-01-14 14:33:40 -0500

    test(dynamomapper): add tests for DynamoIgnore functionality
    - Added `DynamoIgnoreVerifyTests` to validate the behavior of `DynamoIgnore` attributes.
    - Included test cases for various `IgnoreMapping` values: All, FromModel, ToModel.

---
+++ test/LayeredCraft.DynamoMapper.Generators.Tests/DynamoIgnoreVerifyTests.cs
@@ -0,0 +1,41 @@
+namespace LayeredCraft.DynamoMapper.Generators.Tests;
+
+public class DynamoIgnoreVerifyTests
+{
+    [Fact]
+    public async Task DynamoIgnore_Simple() =>
+        await GeneratorTestHelpers.Verify(
+            new VerifyTestOptions
+            {
+                SourceCode = """
+                using System.Collections.Generic;
+                using Amazon.DynamoDBv2.Model;
+                using DynamoMapper.Runtime;
+
+                namespace MyNamespace;
+
+                [DynamoMapper]
+                [DynamoIgnore(nameof(ExampleEntity.Ignore))]
+                [DynamoIgnore(nameof(ExampleEntity.IgnoreAll), Ignore = IgnoreMapping.All)]
+                [DynamoIgnore(nameof(ExampleEntity.IgnoreFrom), Ignore = IgnoreMapping.FromModel)]
+                [DynamoIgnore(nameof(ExampleEntity.IgnoreTo), Ignore = IgnoreMapping.ToModel)]
+                internal static partial class ExampleEntityMapper
+                {
+                    internal static partial Dictionary<string, AttributeValue> ToItem(ExampleEntity source);
+
+                    internal static partial ExampleEntity FromItem(Dictionary<string, AttributeValue> item);
+                }
+
+                internal class ExampleEntity
+                {
+                    internal required string String { get; set; }
+                    internal required string Ignore { get; set; }
+                    internal required string IgnoreAll { get; set; }
+                    internal required string IgnoreFrom { get; set; }
+                    internal required string IgnoreTo { get; set; }
+                }
+                """,
+            },
+            TestContext.Current.CancellationToken
+        );
+}

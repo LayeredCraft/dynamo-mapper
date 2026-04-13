1c75201fc4492781fbecc7e9b4d1e47add7bd69d
Jonas Ha <jonas-ha@outlook.com>
2026-04-13 08:55:11 -0400

    test(generators): cover hook diagnostics and from-only hook scenarios

--- test/LayeredCraft.DynamoMapper.Generators.Tests/HooksVerifyTests.cs
+++ test/LayeredCraft.DynamoMapper.Generators.Tests/HooksVerifyTests.cs
@@ -219,4 +219,196 @@ public class HooksVerifyTests
         },
         TestContext.Current.CancellationToken
     );
+
+    [Fact]
+    public async Task Hooks_FromOnly_BothFromHooks() => await GeneratorTestHelpers.Verify(
+        new VerifyTestOptions
+        {
+            SourceCode =
+                """
+                using System.Collections.Generic;
+                using Amazon.DynamoDBv2.Model;
+                using LayeredCraft.DynamoMapper.Runtime;
+
+                namespace MyNamespace;
+
+                [DynamoMapper]
+                public static partial class ProductMapper
+                {
+                    public static partial Product FromItem(Dictionary<string, AttributeValue> item);
+
+                    static partial void BeforeFromItem(Dictionary<string, AttributeValue> item);
+                    static partial void AfterFromItem(Dictionary<string, AttributeValue> item, ref Product entity);
+                }
+
+                public class Product
+                {
+                    public string Name { get; set; }
+                }
+                """,
+        },
+        TestContext.Current.CancellationToken
+    );
+
+    [Fact]
+    public async Task Hooks_ZeroPropertyToItem_WithBothToHooks() => await GeneratorTestHelpers.Verify(
+        new VerifyTestOptions
+        {
+            SourceCode =
+                """
+                using System.Collections.Generic;
+                using Amazon.DynamoDBv2.Model;
+                using LayeredCraft.DynamoMapper.Runtime;
+
+                namespace MyNamespace;
+
+                [DynamoMapper]
+                public static partial class EmptyModelMapper
+                {
+                    public static partial Dictionary<string, AttributeValue> ToItem(EmptyModel source);
+
+                    static partial void BeforeToItem(EmptyModel source, Dictionary<string, AttributeValue> item);
+                    static partial void AfterToItem(EmptyModel source, Dictionary<string, AttributeValue> item);
+                }
+
+                public class EmptyModel
+                {
+                }
+                """,
+        },
+        TestContext.Current.CancellationToken
+    );
+
+    [Fact]
+    public async Task Hooks_ImplementationOnlyExtendedPartialHook_IsDetected() =>
+        await GeneratorTestHelpers.Verify(
+            new VerifyTestOptions
+            {
+                SourceCode =
+                    """
+                    using System.Collections.Generic;
+                    using Amazon.DynamoDBv2.Model;
+                    using LayeredCraft.DynamoMapper.Runtime;
+
+                    namespace MyNamespace;
+
+                    [DynamoMapper]
+                    public static partial class ProductMapper
+                    {
+                        public static partial Dictionary<string, AttributeValue> ToItem(Product source);
+
+                        private static partial void AfterToItem(Product source, Dictionary<string, AttributeValue> item);
+                    }
+
+                    public static partial class ProductMapper
+                    {
+                        private static partial void AfterToItem(Product source, Dictionary<string, AttributeValue> item) { }
+                    }
+
+                    public class Product
+                    {
+                        public string Name { get; set; }
+                    }
+                    """,
+            },
+            TestContext.Current.CancellationToken
+        );
+
+    [Fact]
+    public async Task Hooks_NonStaticHook_ShouldWarn_DM0402() =>
+        await GeneratorTestHelpers.VerifyFailure(
+            new VerifyTestOptions
+            {
+                SourceCode =
+                    """
+                    using System.Collections.Generic;
+                    using Amazon.DynamoDBv2.Model;
+                    using LayeredCraft.DynamoMapper.Runtime;
+
+                    namespace MyNamespace;
+
+                    [DynamoMapper]
+                    public partial class ProductMapper
+                    {
+                        public partial Dictionary<string, AttributeValue> ToItem(Product source);
+
+                        partial void BeforeToItem(Product source, Dictionary<string, AttributeValue> item);
+                    }
+
+                    public class Product
+                    {
+                        public string Name { get; set; }
+                    }
+                    """,
+                ExpectedDiagnosticId = "DM0402",
+            },
+            TestContext.Current.CancellationToken
+        );
+
+    [Fact]
+    public async Task Hooks_WrongParameterTypes_ShouldWarn_DM0403() =>
+        await GeneratorTestHelpers.VerifyFailure(
+            new VerifyTestOptions
+            {
+                SourceCode =
+                    """
+                    using System.Collections.Generic;
+                    using Amazon.DynamoDBv2.Model;
+                    using LayeredCraft.DynamoMapper.Runtime;
+
+                    namespace MyNamespace;
+
+                    [DynamoMapper]
+                    public static partial class ProductMapper
+                    {
+                        public static partial Dictionary<string, AttributeValue> ToItem(Product source);
+
+                        static partial void BeforeToItem(Order source, Dictionary<string, AttributeValue> item);
+                    }
+
+                    public class Product
+                    {
+                        public string Name { get; set; }
+                    }
+
+                    public class Order
+                    {
+                        public string Id { get; set; }
+                    }
+                    """,
+                ExpectedDiagnosticId = "DM0403",
+            },
+            TestContext.Current.CancellationToken
+        );
+
+    [Fact]
+    public async Task Hooks_InvalidSignature_ShouldWarn_DM0401() =>
+        await GeneratorTestHelpers.VerifyFailure(
+            new VerifyTestOptions
+            {
+                SourceCode =
+                    """
+                    using System.Collections.Generic;
+                    using Amazon.DynamoDBv2.Model;
+                    using LayeredCraft.DynamoMapper.Runtime;
+
+                    namespace MyNamespace;
+
+                    [DynamoMapper]
+                    public static partial class ProductMapper
+                    {
+                        public static partial Dictionary<string, AttributeValue> ToItem(Product source);
+
+                        static partial void BeforeToItem(Product source);
+                    }
+
+                    public class Product
+                    {
+                        public string Name { get; set; }
+                    }
+                    """,
+                ExpectedDiagnosticId = "DM0401",
+            },
+            TestContext.Current.CancellationToken
+        );
 }
